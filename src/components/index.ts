8adf44a2ae9bba331f41f05b2f0534aa34259e31
pedrocruzmay <pedro@builtbyhq.com>
2025-12-22 06:01:16 -0600

    error boundary

--- src/components/index.ts
+++ src/components/index.ts
@@ -1,4 +1,6 @@
 export * from './ErrorMessage';
+export * from './ErrorBoundary';
+export * from './ErrorThrower';
 export * from './LoadingSpinner';
 export * from './TaxCalculatorForm';
 export * from './TaxResults';
